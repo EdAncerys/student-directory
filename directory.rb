4660cb42a00d522aa88a9898cfed506f1efb96c9
Ed <lookatemail@gmail.com>
2020-03-30 17:20:17 +0100

    added comments to directory.rb file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "-" * 31
 puts "The student of Villains Academy"
 puts "-" * 31
@@ -12,6 +13,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finaly we print the total number of students 
 print "Overall, we have"
+# It's important that print() doesn't add new line characters 
 print 11
 puts " great students"
