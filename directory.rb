950f07c7e923b74f0406ff420c40f71504225fda
Ed <lookatemail@gmail.com>
2020-03-30 17:11:42 +0100

    added student list to directory.rb file

---
+++ directory.rb
@@ -0,0 +1,14 @@
+puts "-" * 31
+puts "The student of Villains Academy"
+puts "-" * 31
+puts "Dr. Hannibal Lecter"
+puts "Darh Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch Of The West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
