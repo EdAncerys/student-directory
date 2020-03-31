cf58f7ed8bdc489392595f9ee26b0a2cf60f0ae0
Ed <lookatemail@gmail.com>
2020-03-31 15:56:28 +0100

    added read from file method

--- directory.rb
+++ directory.rb
@@ -28,6 +28,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -55,6 +64,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to student.csv"
+  puts "4. Load the list form students.csv"
   puts "9. Exit" 
   puts "*" * 35
 end
@@ -73,6 +83,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
