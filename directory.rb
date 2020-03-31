653901a8f4f1c9d8fd45189c7266fe4c99498a2f
Ed <lookatemail@gmail.com>
2020-03-31 18:39:04 +0100

    auto load from file feature added

--- directory.rb
+++ directory.rb
@@ -16,6 +16,18 @@ students = [
 # exercise 9 adding an interactive menu
 @students = [] # an emty array accssible to all methods
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # leaves the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
 def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
@@ -25,15 +37,17 @@ def save_students
     csv_line = student_data.join(",")
     file.puts csv_line
   end
+  puts "Student name list saved to file"
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
+  puts "Name list uploaded from #{filename}"
   file.close
 end
 
@@ -41,21 +55,21 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp 
+    name = STDIN.gets.chomp 
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -119,4 +133,5 @@ end
 # print_header
 # print(students)
 # print_footer(students)
+try_load_students
 interactive_menu
